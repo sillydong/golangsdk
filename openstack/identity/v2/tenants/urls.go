421e6a46e2c0744c66a03cc539668c43e2bc7976
Chen.Zhidong <zhidong.chen@jimeng.io>
2018-12-29 00:21:23 +0800

    add identity v2 doc

--- openstack/identity/v2/tenants/urls.go
+++ openstack/identity/v2/tenants/urls.go
@@ -2,22 +2,27 @@ package tenants
 
 import "github.com/huaweicloud/golangsdk"
 
+// listURL generates URL for listing tenants.
 func listURL(client *golangsdk.ServiceClient) string {
 	return client.ServiceURL("tenants")
 }
 
+// getURL generates URL for getting tenant.
 func getURL(client *golangsdk.ServiceClient, tenantID string) string {
 	return client.ServiceURL("tenants", tenantID)
 }
 
+// createURL generates URL for createing tenant.
 func createURL(client *golangsdk.ServiceClient) string {
 	return client.ServiceURL("tenants")
 }
 
+// deleteURL generates URL for deleteing tenant.
 func deleteURL(client *golangsdk.ServiceClient, tenantID string) string {
 	return client.ServiceURL("tenants", tenantID)
 }
 
+// updateURL generates URL for updating a tenant.
 func updateURL(client *golangsdk.ServiceClient, tenantID string) string {
 	return client.ServiceURL("tenants", tenantID)
 }
